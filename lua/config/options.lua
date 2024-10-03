7b392f6ab5ab9516c1893469604851e39173f1d6
isaac-varg <hello@isaacvargas.dev>
2024-10-02 20:36:37 -0700

    inital commit

---
+++ lua/config/options.lua
@@ -0,0 +1,30 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.wrap = false
+
+vim.opt.expandtab = true
+-- vim.opt.tabstop = 4
+-- vim.opt.shiftwidth = 4
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.scrolloff = 999
+
+-- this is so that visual block mode can move beyond the line
+vim.opt.virtualedit = "block"
+
+-- when doing a command like /%s/x/y shows changes throughout entire file below
+vim.opt.inccommand = "split"
+
+-- ignore casing throughout editor, e.g., : complete
+vim.opt.ignorecase = true
+
+-- allow true color support of modern terminal emulators
+vim.opt.termguicolors = false 
