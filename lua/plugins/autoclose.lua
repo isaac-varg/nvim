01434af39914fe30af5cf9f45e8077d897114ce7
isaac-varg <hello@isaacvargas.dev>
2024-10-06 21:35:17 -0700

    add autoclose

---
+++ lua/plugins/autoclose.lua
@@ -0,0 +1,9 @@
+-- lightweight autoclose/autopair plugin
+-- has no tree sitter integrations
+-- might switch out for -- lightweight autoclose/autopair plugin
+-- has no tree sitter integrations https://github.com/windwp/nvim-autopairs
+-- but this one was simplet to set up
+return {
+        "m4xshen/autoclose.nvim",
+        opts = { }
+}
